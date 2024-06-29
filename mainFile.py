83e13028aaf333a5a57e5d8ee4c4c730d4852bf4
Parsa Ghofrani <parsa.ghfrni@gmail.com>
2024-06-29 17:21:10 +0330

    adding the mux

--- mainFile.py
+++ mainFile.py
@@ -24,3 +24,18 @@ def NOR_GATE(a, b):
     return NOT_GATE(OR_GATE(a,b))
 
 
+def MUX(n, select_lines, data_lines):
+    output = 0
+
+    # Iterating over combinations of select lines
+    for i in range(n):
+        # Checking if the current select line combination matches the selection and selecting it
+        if all(select_lines[j] == ((i >> j) & 1) for j in range(len(select_lines))):
+            output = data_lines[i]
+
+    return output
+
+
+def SUB():
+    return
+    #TODO
