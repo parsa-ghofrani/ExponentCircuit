1989a66fa53f8d3de94f53a943a199d2f58e9932
Parsa Ghofrani <parsa.ghfrni@gmail.com>
2024-06-29 11:37:43 +0330

    initial commit

--- mainFile.py
+++ mainFile.py
@@ -1,29 +1,26 @@
 # defining the gates we need in the project :
 
 # 1 bit not gate
-def NOT_GATE(input_bit):
-    return int(not input_bit)
+def NOT_GATE(a):
+    return ~a + 1
 
 
 # n bits and gate
-def AND_GATE(input_bit):
-    return
-    # TODO
+def AND_GATE(a, b):
+    return a & b
 
 
 # n bits or gate
-def OR_GATE(input_bit):
-    return
-    # TODO
+def OR_GATE(a, b):
+    return a | b
 
 
 # n bits xor gate
-def XOR_GATE(input_bit):
-    # TODO
-    return
+def XOR_GATE(a,b):
+    return a ^ b
 
 
-def NOR_GATE(input_bit):
-    return
-    # TODO
+def NOR_GATE(a, b):
+    return NOT_GATE(OR_GATE(a,b))
+
 
