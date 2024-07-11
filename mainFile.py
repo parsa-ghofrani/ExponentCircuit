e769381959b1eb259f42fa7daa7797f1833735b5
Parsa Salamatipour <sa.parsa20@gmail.com>
2024-07-11 19:08:07 +0330

    Fixed IntSqrt, Added FracSqrt

--- mainFile.py
+++ mainFile.py
@@ -18,13 +18,36 @@ class Wire:
         if isinstance(key, int):
             if key < 0:
                 key += self.bit_count
+            if key < 0 or key >= self.bit_count:
+                raise IndexError("Wire index out of range")
             return Wire((self.value >> key) & 1, 1)
         elif isinstance(key, slice): # TODO: implement slice for negative values
             start = key.start or 0
             stop = key.stop or (self.bit_count + 1)
-            stop -= 1
+            # stop -= 1
             mask = (1 << (stop - start)) - 1
             return Wire((self.value >> start) & mask, stop - start)
+    
+
+    def __setitem__(self, key, value):
+        if not isinstance(value, Wire):
+            raise ValueError("Value must be a Wire instance")
+        if isinstance(key, int):
+            if key < 0:
+                key += self.bit_count
+            if key < 0 or key >= self.bit_count:
+                raise IndexError("Wire index out of range")
+            if value.value == 1:
+                self.value |= (1 << key)
+            else:
+                self.value &= ~(1 << key)
+        elif isinstance(key, slice):
+            start = key.start or 0
+            stop = key.stop or (self.bit_count + 1)
+            mask = (1 << (stop - start)) - 1
+            self.value = (self.value & ~(mask << start)) | (value.value << start)
+        else:
+            raise TypeError("Invalid argument type")
     
     def __and__(self, other):
         if isinstance(other, Wire):
@@ -79,6 +102,10 @@ class Wire:
         a = self
         b = other if isinstance(other, Wire) else Wire(other, a.bit_count)
         n = max(a.bit_count, b.bit_count)
+        if a.bit_count < b.bit_count:
+            a = Wire(0, b.bit_count - a.bit_count) // a
+        elif b.bit_count < a.bit_count:
+            b = Wire(0, a.bit_count - b.bit_count) // b
         result = Wire(0, n)
         borrow = Wire(0, 1)
 
@@ -98,8 +125,8 @@ class Wire:
     def concat(self, other):
         return Wire((self.value << other.bit_count) | other.value, self.bit_count + other.bit_count)
 
-    def __floordiv__(self, other):
-        self.concat(other)
+    def __floordiv__(self, other): # // is concatenation
+        return self.concat(other)
 
     def reverse(self):
         mask = 1 << (self.bit_count - 1)  # Mask to isolate the MSB
@@ -125,51 +152,36 @@ def MUX(data_lines: list[Wire], select_lines: Wire) -> Wire:
     return output
 
 def SubPos(a: Wire, b: Wire) -> Wire:
-    b, s = a - b
-    return MUX([a, s], ~b)
+    borrow, s = a - b
+    return ~borrow, MUX([s, a], borrow)
 
 
 def IntSqrt(input: Wire) -> Wire:
+    if input.bit_count % 2 == 1:
+        input = Wire(0,1) // input
+    
     n = input.bit_count
     m = n // 2
 
-    groups = []
-    if n % 2 == 0:
-        for i in range(0,n,2):
-            groups.append(input[i:i+1])
-    else:
-        for i in range(0,n-1,2):
-            groups.append(input[i:i+1])
+    # Initialize the output bits
+    q = Wire(0, m)
+    pre_layer = input[n-2:n] # last two bits
+    for i in range(m-1, -1, -1):
+        if i == m - 1:
+            q[i], pre_layer = SubPos(pre_layer, Wire(0b01,2))
+        else:
+            pre_layer = pre_layer // input[2*i : 2*i + 2]
+            q[i], pre_layer = SubPos(pre_layer, q[i+1:m] // Wire(0b01,2))
 
-        groups.append(Wire(0,1) // (input[n-1]))
-        # Initialize the output bits
-        q = [Wire(0, 1) for _ in range(m)]
-        for i in range(m-1,-1,-1):
-            last_half_groups = groups[-len(q):]
-            last_half_q = q[-len(last_half_groups):]
-            for group, q_bit in zip(last_half_groups, last_half_q):
-                if group.value >= q_bit.value << (group.bit_count - q_bit.bit_count - 1):
-                    groups[m-1:i] = SubPos(groups[m-1:i],q[m-1:i+1]//Wire(0b01,2))# FIXME: implement the sudocode provided in the pdf
-                    q[i] = Wire(1,1)
-                else:
-                    q[i] = Wire(0,1)
-
-    """the previous code:Iterate over each group and each bit position in reverse order
     
-    for i in range(m - 1, -1, -1):
-        for j in range(len(groups) - 1, -1, -1):
-            # Update the output bits
-            if groups[j].value >= output_bits[i].value << (j * m):
-                groups[j] = SubPos(groups[j], output_bits[i])
-                output_bits[i] = Wire(1, 1)
-            else:
-                output_bits[i] = Wire(0, 1)"""
+    return q
 
+def FracSqrt(input: Wire) -> Wire:
+    return IntSqrt(input // Wire(0, input.bit_count))
 
-    """for i in range(m - 1, -1, -2):
-        output = Wire.concat(output_bits[i], output_bits[i-1])
-
-    return output"""
+def FloatSqrt(input: Wire) -> Wire:
+    pass
+    # TODO
 
 
 
@@ -178,12 +190,21 @@ def IntSqrt(input: Wire) -> Wire:
 # Test cases
 a = Wire(0b1011, 4)
 b = Wire(0b0101, 4)
-print(a)
-print(a[-4])
-print(a[0:3])
-print(a - b)  # 5
-print(a + b)  # 15
-print(a.concat(b))  # 0b10100101
-print("HI")
-print(IntSqrt(Wire(0b0001,4))) #0b0010
-print(MUX([a, b], Wire(0, 1)))  # 0b1010
+print(f'{a = }')
+print(f'{a[-4] = }')
+print(f'{a[0:3] = }')
+print(f'{a - b = }')  # 5
+print(f'{a + b = }')  # 15
+print(f'{a.concat(b) = }')  # 0b10100101
+
+w = Wire(0b1010, 4)
+print(f'{w = }')  # Wire(value=10, bit_count=4)
+w[2] = Wire(1, 1)
+print(f'w after w[2] <- 1 = {w}')  # Wire(value=2, bit_count=4)
+
+c = Wire(0b101, 3)
+d = Wire(0b1, 1)
+print(f'{c - d = }') # 0b100
+
+print(f'{IntSqrt(Wire(0b1010010100000000,16)) = }') # 0b11001101
+print(f'{MUX([a, b], Wire(0, 1)) = }')  # 0b1010
